006ef492ef5d0c36cc015e99bea9b1c37552b524
haosu <me@haosu.org>
2014-03-05 23:16:22 -0800

    Add basic Goal creation

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,21 +1,50 @@
 class GoalsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_goal, except: %i(index create)
 
   def index
+  end
+
+  def show
   end
 
   def new
   end
 
   def create
+    goal = Goal.new(goal_params) do |g|
+      g.owner = current_user
+    end
+
+    if goal.save
+      redirect_to goal
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    render 'edit'
   end
 
   def destroy
+  end
+
+private
+
+  def set_goal
+    @goal =
+      if id = params[:id]
+        Goal.find(id)
+      else
+        Goal.new
+      end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:name, :reason, :critical_mass)
   end
 end
