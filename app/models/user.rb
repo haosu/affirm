75db4609120b45d0eaed0fe7d8e99108a960ea51
haosu <me@haosu.org>
2014-03-05 23:39:28 -0800

    Add relationships to Goal and Affirmation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  has_many :goals, foreign_key: :owner_id
+  has_many :affirmations, foreign_key: :affirmer_id
+
   def set_default_role
     self.role ||= :user
   end
