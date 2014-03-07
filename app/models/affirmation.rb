fc885ad3d902717f51e032c154eb3617e4fe7aef
haosu <me@haosu.org>
2014-03-06 22:36:31 -0800

    Add affirmations association to goal, and fix typo

--- app/models/affirmation.rb
+++ app/models/affirmation.rb
@@ -1,4 +1,4 @@
-class Affirmation < ActivitRecord::Base
+class Affirmation < ActiveRecord::Base
   belongs_to :affirmer, class_name: 'User'
   belongs_to :goal
 
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,8 @@
 class Goal < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
 
+  has_many :affirmations
+
   validates :owner,         presence: true
   validates :name,          presence: true
   validates :reason,        presence: true
