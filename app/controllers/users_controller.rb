2fecd13bb8b18b8ada0023333056b6260d3e7b61
haosu <me@haosu.org>
2014-03-07 16:25:04 -0800

    Add goal list to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @goals = Goal.where(owner_id: @user.id)
+
     unless current_user.admin?
       unless @user == current_user
         redirect_to root_path, :alert => "Access denied."
